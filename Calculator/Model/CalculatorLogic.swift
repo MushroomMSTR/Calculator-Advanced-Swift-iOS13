1b4c7001b53b6ae3742ac7e168cba40b8462d98f
NazarStf <hworker98@gmail.com>
2023-05-27 02:21:07 +0300

    Added other function buttons for calculation, such as "÷", "×", "-", "+" "="

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -9,22 +9,36 @@ import Foundation
 
 class CalculatorLogic {
 	private var displayText: String = "0"
+	private var firstOperand: Double?
+	private var secondOperand: Double?
+	private var currentOperator: String?
 	
 	func handleButtonPress(_ buttonTitle: String) -> String {
 		switch buttonTitle {
 		case "AC":
-			displayText = "0"
+			clear()
 		case "+/-":
 			toggleSign()
 		case "%":
 			calculatePercentage()
 		case ".":
 			addDecimalPoint()
+		case "÷", "×", "-", "+":
+			handleOperator(buttonTitle)
+		case "=":
+			calculateResult()
 		default:
 			appendNumber(buttonTitle)
 		}
 		
 		return displayText
+	}
+	
+	private func clear() {
+		displayText = "0"
+		firstOperand = nil
+		secondOperand = nil
+		currentOperator = nil
 	}
 	
 	private func toggleSign() {
@@ -56,8 +70,39 @@ class CalculatorLogic {
 		}
 	}
 	
+	private func handleOperator(_ operatorSymbol: String) {
+		if let number = Double(displayText) {
+			firstOperand = number
+			currentOperator = operatorSymbol
+			displayText = "0"
+		}
+	}
+	
+	private func calculateResult() {
+		if let firstNumber = firstOperand, let secondNumber = Double(displayText), let operation = currentOperator {
+			var result: Double
+			
+			switch operation {
+			case "÷":
+				result = firstNumber / secondNumber
+			case "×":
+				result = firstNumber * secondNumber
+			case "-":
+				result = firstNumber - secondNumber
+			case "+":
+				result = firstNumber + secondNumber
+			default:
+				return
+			}
+			
+			displayText = formatResult(result)
+			firstOperand = nil
+			secondOperand = nil
+			currentOperator = nil
+		}
+	}
+	
 	private func formatResult(_ number: Double) -> String {
 		return String(number)
 	}
 }
-
